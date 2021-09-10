57df71e5c5d1507c9434dd9661cd8d6ee0266338
Almego <alvaromerinogar@gmail.com>
2021-09-10 13:00:28 +0200

    progress index

--- my-app/src/components/Header/Header.js
+++ my-app/src/components/Header/Header.js
@@ -1,13 +1,19 @@
+import { requirePropFactory } from "@material-ui/core";
 import React from "react";
 import { NavLink } from "react-router-dom";
-
+import inmobiliariaLogo from "../../image/inmobiliariaLogo.png";
 import "./Header.scss";
 
 function Header({ ...props }) {
   return (
     <header className="bg-primary mb-4 Header" {...props}>
       <div className="container-fluid">
-        <div className="row">
+        <div className="row div-container">
+          <nav className="navbar navbar-expand navbar-dark nav-container">
+            <div>
+              <img src={inmobiliariaLogo} />
+            </div>
+          </nav>
           <nav className="navbar navbar-expand navbar-dark nav-container">
             <NavLink
               exact
@@ -16,15 +22,15 @@ function Header({ ...props }) {
               to="/"
             >
               Home
-              </NavLink>
-              <NavLink
-                  exact
-                  activeClassName="active"
-                  className="nav-link  navLink-space"
-                  to="/Dashboard"
-                >
-                  Dashboard
-              </NavLink>
+            </NavLink>
+            <NavLink
+              exact
+              activeClassName="active"
+              className="nav-link  navLink-space"
+              to="/Dashboard"
+            >
+              Dashboard
+            </NavLink>
           </nav>
         </div>
       </div>
--- my-app/src/pages/Home/Home.js
+++ my-app/src/pages/Home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "./style.scss";
 import SearchBar from "../../components/SearchBar";
 import withLayout from "../../hoc/withLayout";
 
@@ -8,8 +8,12 @@ function Home ({
     ...props
 }) {
     return (
-        <div>
-            Hello World
+        <div className="gridForm">
+            <div>
+                <p>
+                Lorem Ipsum es simplemente texto de relleno de la industria de la impresión y la composición tipográfica. Lorem Ipsum ha sido el texto de relleno estándar de la industria desde la década de 1500, cuando un impresor desconocido tomó una galera de tipos y la mezcló para hacer un libro de muestras tipográficas. Ha sobrevivido no solo a cinco siglos, sino también al salto a la composición tipográfica electrónica, permaneciendo esencialmente sin cambios. Se popularizó en la década de 1960 con el lanzamiento de hojas de Letraset que contenían pasajes de Lorem Ipsum y, más recientemente, con software de autoedición como Aldus PageMaker que incluía versiones de Lorem Ipsum. 
+                </p>
+            </div>    
             <div>
                 <SearchBar
                     type="text"
